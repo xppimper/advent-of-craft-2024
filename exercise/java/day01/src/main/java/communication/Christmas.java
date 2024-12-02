92b7c915869b6c06670f5adb51a6db778b0a9bc9
Chams Eddine CISSE <chams-eddine-externe.cisse@edf.fr>
2024-12-02 16:07:34 +0100

    fix: Remove unused code - day 01

--- exercise/java/day01/src/main/java/communication/Christmas.java
+++ exercise/java/day01/src/main/java/communication/Christmas.java
@@ -1,21 +1,7 @@
 package communication;
 
-import java.time.Clock;
-import java.time.Instant;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-
 public class Christmas {
     private final int numberOfDaysBeforeChristmas;
-
-    public Christmas(Clock clock) {
-        Date currentDate = Date.from(Instant.now(clock));
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(currentDate);
-        Calendar christmas = new GregorianCalendar(calendar.get(Calendar.YEAR), Calendar.DECEMBER, 25);
-        numberOfDaysBeforeChristmas = christmas.get(Calendar.DAY_OF_YEAR) - calendar.get(Calendar.DAY_OF_YEAR);
-    }
 
     private Christmas(int numberOfDaysBeforeChristmas) {
         this.numberOfDaysBeforeChristmas = numberOfDaysBeforeChristmas;
